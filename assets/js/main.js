339add2bc71642356b7e03865c5e24a2572b2110
Jonathan Lopes <jonathanfiles10@gmail.com>
2022-08-19 04:52:12 -0300

    Adicionando tarefa

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,83 +1,50 @@
-const inputTarefa = document.querySelector(".input-tarefa");
-const btnTarefa = document.querySelector(".btn-tarefa");
-const tarefas = document.querySelector(".tarefas");
+// Selecionando os elementos
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
 
-function criaLi(){
-    const li = document.createElement('li');
-    return li;
+
+// Funções 
+const saveTodo = (text) => {
+    const todo = document.createElement("div");
+    todo.classList.add("todo");
+
+    const todoTitle = document.createElement("h3");
+    todoTitle.innerText = text;
+    todo.appendChild(todoTitle);
+
+    const doneBtn = document.createElement("button");
+    doneBtn.classList.add("finish-todo");
+    doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+    todo.appendChild(doneBtn);
+
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("edit-todo");
+    editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+    todo.appendChild(editBtn);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("remove-todo");
+    deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+    todo.appendChild(deleteBtn);
+
+    todoList.appendChild(todo);
+
+    todoInput.value = "";
+    todoInput.focus();
 }
 
-// Aqui nós utilizando o eventListener para adicionar uma tarefa utilizando o Enter, utilizando uma verificação para que se o evento de keypress for igual o keycode 13 (Enter) ele realizar a tarefa.
-inputTarefa.addEventListener("keypress", function(e){
-    if(e.keyCode === 13){
-        if(!inputTarefa.value) return;
-        criaTarefa(inputTarefa.value);
+
+// Eventos
+todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    const inputValue = todoInput.value;
+
+    if(inputValue){
+        saveTodo(inputValue);
     }
 });
-
-function limpaInput(){
-    inputTarefa.value = ''; // Função criada para limpar o input após adicionar uma tarefa
-    inputTarefa.focus(); // O focus aqui serve para retornar o cursor piscando dentro do input utilizado
-}
-
-function criaBotaoApagar(li){
-    li.innerText += ' ';
-    const botaoApagar = document.createElement('button');
-    botaoApagar.innerText = 'Apagar';
-    const iconeApagar = document.createElement('i');
-    iconeApagar.setAttribute('class', 'fa fa-trash');
-    botaoApagar.setAttribute('class', 'apagar'); //Aqui nós estamos adicionando uma classe através do setAttribute. Nele nós passamos 2 parâmetros, o atributo e o valor. Nesse caso passamos que será adicionado uma classe e logo a frente o nome da classe adicionada.
-    botaoApagar.setAttribute('title', 'Apagar tarefa'); // Outro exemplo utilizando setAttribute, que pode ser utilizado de diversas maneiras.
-    li.appendChild(botaoApagar);
-    botaoApagar.appendChild(iconeApagar);
-}
-
-function criaTarefa(textoInput){
-    const li = criaLi(); // No caso dessa variável li, ela não é a mesma da outra que cria o elemento li no HTML pois estão em escopos de funções diferentes.
-    li.innerText = textoInput;
-    tarefas.appendChild(li); // Aqui usando o apprendChild nós estamos inserindo o li dentro da ul tarefas.
-    limpaInput();
-    criaBotaoApagar(li);
-    salvarTarefas();
-}
-// Dentro da função criaTarefa nós estamos chamando outras funções, pois quando a criação for executada, junto dela é para ser criado as outras funcionalidades como o botão de apagar por exemplo.
-
-
-btnTarefa.addEventListener("click", function(){
-    if(!inputTarefa.value) return;
-    criaTarefa(inputTarefa.value);
-});
-
-document.addEventListener("click", function(e){
-    const elemento = e.target;
-
-    if(elemento.classList.contains('apagar')){
-        elemento.parentElement.remove(); //Aqui nós temos a seguinte condição: Se o elemento clicado conter a classe 'apagar', o pai dele será removido. No caso ao clicar no botão apagar, o pai dele (li) será apagado.
-        salvarTarefas();
-    }
-});
-
-function salvarTarefas(){
-    const liTarefas = tarefas.querySelectorAll("li"); // Selecionando todos elementos dentro da ul tarefas.
-    const listaDeTarefas = [];
-
-    for(let tarefa of listaDeTarefas){
-        let TarefaTexto = tarefa.innerText;
-        TarefaTexto = TarefaTexto.replace('Apagar', '').trim();
-        listaDeTarefas.push(TarefaTexto); // Adicionando o texto inserido no input dentro do array
-    }
-
-    const tarefasJSON = JSON.stringify(listaDeTarefas);
-    localStorage.setItem('tarefas', tarefasJSON);
-}
-
-function adicionaTarefasSalvas(){
-    const tarefas = localStorage.getItem('tarefas');
-    const listaDeTarefas = JSON.parse(tarefas);
-
-    for(let tarefa of listaDeTarefas){
-        criaTarefa(tarefa);
-    }
-}
-
-adicionaTarefasSalvas();
